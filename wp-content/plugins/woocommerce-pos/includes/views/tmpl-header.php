1a4101aa00e4d14ba1195ca9c53221baea09767f
harshal.avantsoft <harshal.avantsoft@gmail.com>
2018-10-03 10:23:55 -0600

    added pos plugin and update mailchimp

---
+++ wp-content/plugins/woocommerce-pos/includes/views/tmpl-header.php
@@ -0,0 +1,20 @@
+<?php
+  // using global user info
+  global $current_user;
+?>
+<div>
+  <a href="#" id="menu-btn"><i class="icon-bars"></i> <span><?php /* translators: wordpress */ _e( 'Menu' ); ?></span></a>
+</div>
+<img src="<?php echo wp_get_attachment_url("6"); ?>" width="auto" height="40" />
+<!-- <h1 class="center-block">{{name}}</h1> -->
+<div>
+  <a href="#" data-toggle="dropdown">
+    <?php echo get_avatar( $current_user->ID, 20 ); ?>
+    <span><?php echo $current_user->display_name ?></span>
+  </a>
+  <div class="dropdown-list">
+    <ul>
+      <li><a href="<?php echo wp_logout_url( home_url() ); ?>"><?php /* translators: wordpress */ _e( 'Log Out' ); ?></a></li>
+    </ul>
+  </div>
+</div>
