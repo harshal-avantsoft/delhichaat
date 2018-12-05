3ea20b8c9afd1083303ae30a5ff4ce6285d9df04
harshal.avantsoft <harshal.avantsoft@gmail.com>
2018-12-05 09:43:39 -0600

    plugins update

--- wp-content/plugins/woocommerce-pdf-invoices-packing-slips/includes/documents/class-wcpdf-invoice.php
+++ wp-content/plugins/woocommerce-pdf-invoices-packing-slips/includes/documents/class-wcpdf-invoice.php
@@ -29,7 +29,7 @@ class Invoice extends Order_Document_Methods {
 	public function __construct( $order = 0 ) {
 		// set properties
 		$this->type		= 'invoice';
-		$this->title	= __( 'Receipt', 'woocommerce-pdf-invoices-packing-slips' );
+		$this->title	= __( 'Invoice', 'woocommerce-pdf-invoices-packing-slips' );
 		$this->icon		= WPO_WCPDF()->plugin_url() . "/assets/images/invoice.png";
 
 		// Call parent constructor
@@ -49,7 +49,7 @@ class Invoice extends Order_Document_Methods {
 
 	public function get_title() {
 		// override/not using $this->title to allow for language switching!
-		return apply_filters( "wpo_wcpdf_{$this->slug}_title", __( 'Receipt', 'woocommerce-pdf-invoices-packing-slips' ), $this );
+		return apply_filters( "wpo_wcpdf_{$this->slug}_title", __( 'Invoice', 'woocommerce-pdf-invoices-packing-slips' ), $this );
 	}
 
 	public function init() {
@@ -63,6 +63,10 @@ class Invoice extends Order_Document_Methods {
 
 		$this->set_date( current_time( 'timestamp', true ) );
 		$this->init_number();
+	}
+
+	public function exists() {
+		return !empty( $this->data['number'] );
 	}
 
 	public function init_number() {
